c6dba396c52c70051c5e2cf9571728dde29ce6b7
David White <davewx7@gmail.com>
2026-04-04 23:39:59 -0700

    fix bug where respite activity wasn't network syncing

--- Draw Steel Core Rules/MCDMInitiativeBar.lua
+++ Draw Steel Core Rules/MCDMInitiativeBar.lua
@@ -1911,13 +1911,21 @@ function GameHud.CreateRespiteBar(self, info)
 			valign = "Bottom",
 			fontSize = 22,
 			press = function(element)
+                local groupid = dmhub.GenerateGuid()
 				if not CanControlInitiative() then return end
 				if info.initiativeQueue ~= nil then
 					info.initiativeQueue.gameMode = "exploration"
 					info.UploadInitiative()
 					for _, token in pairs(dmhub.GetTokens({playerControlled = true})) do
 						local currentXp = token.properties:try_get("xp", 0)
-						token.properties:Rest("long")
+                        token:ModifyProperties{
+                            description = "Respite",
+                            combine = true,
+                            groupid = groupid,
+                            execute = function()
+						        token.properties:Rest("long")
+                            end,
+                        }
 						local newXp = token.properties:try_get("xp", 0)
 
 						token.properties:DispatchEvent("endrespite", {xpgained = newXp - currentXp})
